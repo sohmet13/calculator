0b3f9fc7a4b3c3ff4a0b67fc5168e78bdf1693ec
Лощакова Анна Александровна <17537870@sigma.sbrf.ru>
2019-10-08 18:52:29 +0300

    set configuration

--- src/script.js
+++ src/script.js
@@ -1,24 +1,27 @@
-$(document).ready(function() { 
+import $ from 'jquery';
+import './style.scss';
+
+$(document).ready(function() {
   var result = 0;
   $('.button').on('mousedown', function(){
     const button = $(this).text();
     const head = $('#main');
     const str = $('#expression');
     //проверяем, что нажатая копка с цифрой
-     if (button.match(/[0-9]/)){
-       //проверяем что в заголовке не 0 и в строке выражения, не математические знаки и это не законченное выражение
-       if ((head.text()==='0' && (str.text()==='0' || str.text()==='Digit Limit Met')) || head.text().match(/[/*+]|-/) || str.text().match(/=/g)){
+    if (button.match(/[0-9]/)){
+      //проверяем что в заголовке не 0 и в строке выражения, не математические знаки и это не законченное выражение
+      if ((head.text()==='0' && (str.text()==='0' || str.text()==='Digit Limit Met')) || head.text().match(/[/*+]|-/) || str.text().match(/=/g)){
         head.text(button);
-       } else {
-         head.text(head.text()+button);
-       }
-       //проверяем что в строке выражения не ноль, не превышение длины строки и не законченное выражение
-       if (str.text()==='Digit Limit Met' || str.text()==='0' || str.text().match(/[=]/g)){
-          str.text(button);
-       } else {
-         str.text(str.text()+button);
-       }
-     }
+      } else {
+        head.text(head.text()+button);
+      }
+      //проверяем что в строке выражения не ноль, не превышение длины строки и не законченное выражение
+      if (str.text()==='Digit Limit Met' || str.text()==='0' || str.text().match(/[=]/g)){
+        str.text(button);
+      } else {
+        str.text(str.text()+button);
+      }
+    }
     //точки
     if (button==='.') {
       head.text(head.text()+button);
@@ -38,39 +41,39 @@ $(document).ready(function() {
     }
     //выполняем математические операции
     if(button.match(/[/*+]|-/)) {
-        head.text(button);
+      head.text(button);
       //проверяем что в подстрочке все в порядке
       if (str.text()==='Digit Limit Met') {
-         str.text(button);
+        str.text(button);
         //что в ней нет знака равно
-       } else if (str.text().match(/[=]/g)) {
-         str.text(result+button);
-         //заменям существующий оператор на нажатый
-       } else if (str.text()[str.text().length-1].match(/[/*+]|-/)) {
-         str.text(str.text().slice(0, -1)+button);
-         //если обошли все трудные случаи - просто вставлем текст
-       } else {
-         str.text(str.text()+button);
-       }
+      } else if (str.text().match(/[=]/g)) {
+        str.text(result+button);
+        //заменям существующий оператор на нажатый
+      } else if (str.text()[str.text().length-1].match(/[/*+]|-/)) {
+        str.text(str.text().slice(0, -1)+button);
+        //если обошли все трудные случаи - просто вставлем текст
+      } else {
+        str.text(str.text()+button);
+      }
     }
     //что делать после равно
     if (button==='=') {
       result = eval(str.text());
       if (result!==Math.round(result)) {
-       if (result==result.toFixed(1)){ 
+        if (result==result.toFixed(1)){ 
           result = result.toFixed(1);
-       } else {
+        } else {
           result = result.toFixed(2); 
-       }
+        }
       }
       head.text(result);
       str.text(str.text()+'='+result);
     }
     //очищаем обе строчки
     if (button==='AC'){
-        head.text(0);
-        str.text(0);
-     }
+      head.text(0);
+      str.text(0);
+    }
     //убираем последнее введенное значение 
     if (button==='CE') {
       if (str.text().match(/[=]/g)) {
@@ -82,10 +85,10 @@ $(document).ready(function() {
       var change = str.text();
       //меняем параметры поиска в зависимости от того, чем заканчивается выражение
       if (str.text()[str.text().length-1].match(/[/*+]|-/)) {
-          text(/[0-9.]/)
-        } else {
-          text(/[/*+]|-/);
-        }
+        text(/[0-9.]/)
+      } else {
+        text(/[/*+]|-/);
+      }
       //ищем либо последний оператор либо последнее число в строке выражения для его отображения в главном поле ввода чисел/операторов
       function text(re) {
         let string = change;
