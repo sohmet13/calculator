d97dc3909ccb049e2763262733b395906b631ff7
Лощакова Анна Александровна <17537870@sigma.sbrf.ru>
2019-10-17 16:08:04 +0300

    refactor project

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,7 +13,7 @@ export class Calculator extends Component {
     }
   }
 
-  pressNumbers(num) {
+  pressNumbers = (num) => {
     // мы не удаляем предыдущее выражение, если оно не 0, не текст о превышении лимита, и и не оконченное выражение (т.е. с равно)
     const saveExpression =
             !['0', this.state.limit].includes(this.state.expression) && !this.state.expression.match(this.state.equalSignMatcher);
@@ -24,9 +24,9 @@ export class Calculator extends Component {
     const expression = `${saveExpression ? this.state.expression: ''}${num}`;
 
     this.setState({expression, inputValue}, this.checkLimit);
-  }
+  };
 
-  pressPoint() {
+  pressPoint = () => {
     const getValues = () => {
       switch (true) {
       case !!this.state.inputValue.match(this.state.mathSignsMatcher):
@@ -52,9 +52,9 @@ export class Calculator extends Component {
       }
     };
     this.setState(getValues(), this.checkLimit);
-  }
+  };
 
-  pressMathSigns(sign) {
+  pressMathSigns = (sign) => {
     const getExpression = () => {
       switch (true) {
       case this.state.expression === this.state.limit:
@@ -69,9 +69,12 @@ export class Calculator extends Component {
       }
     };
     this.setState({expression: getExpression(), inputValue: sign}, this.checkLimit);
-  }
+  };
 
-  pressEqualSign() {
+  pressEqualSign = () => {
+    if (this.state.expression[this.state.expression.length - 1].match(this.state.mathSignsMatcher)) {
+      return;
+    }
     // eval - это, конечно, зло, но он именно для таких выражений он и создавался и здесь он безопасен
     // eslint-disable-next-line no-eval
     let result = eval(this.state.expression);
@@ -82,53 +85,53 @@ export class Calculator extends Component {
       expression: `${this.state.expression}=${result}`,
       inputValue: result
     }, this.checkLimit)
-  }
+  };
 
-  reset() {
+  reset = () => {
     this.setState({
       expression: '0',
       inputValue: '0'
     })
-  }
+  };
 
   // удаляет последнее имеющее смысл выражение в строке (т.е. либо набор чисел с точкой до мат.знака, либо сам мат.знак)
-  deleteLast() {
+  deleteLast = () => {
     if (this.state.expression.match(this.state.equalSignMatcher)) {
       this.reset();
       return;
     }
     const newExpression = this.state.expression.slice(0, -this.state.inputValue.length) || '0';
     // обрезает выражение с конца по одному знаку до тех пор, пока последний символ не будет соотвествовать проброшенному regExp
-    const getInputValue = (regExp, change = newExpression) =>
-      change.match(regExp) ? getInputValue(regExp, change.slice(change.search(regExp) + 1)) : change || '0';
+    const getInputValue = (regExp, newInputValue = newExpression) =>
+      newInputValue.match(regExp) ? getInputValue(regExp, newInputValue.slice(newInputValue.search(regExp) + 1)) : newInputValue || '0';
     this.setState({
       // если выражение оканчивается на число или точку, то надо обрезать до математического знака, и наоборот
       inputValue: getInputValue(newExpression[newExpression.length - 1]
         .match(this.state.mathSignsMatcher) ? /[0-9.]/ : this.state.mathSignsMatcher),
       expression: newExpression
     }, this.checkLimit);
-  }
+  };
 
-  checkLimit() {
+  checkLimit = () => {
     if (this.state.inputValue.length > 9 || this.state.expression.length > 25) {
       this.setState({
         expression: this.state.limit,
         inputValue: '0'
       });
     }
-  }
+  };
 
   render() {
     return (
       <div className='Calc'>
         <h1 className="Calc__title">ELECTRONIC CALCULATOR</h1>
-        <div className='Screen'>
-          <div className='Screen__text Screen__input-value'>{this.state.inputValue}</div>
-          <div className='Screen__text Screen__expression'>{this.state.expression}</div>
+        <div className='Calc__screen'>
+          <div className='Calc__screen-text Calc__input-value'>{this.state.inputValue}</div>
+          <div className='Calc__screen-text Calc__expression'>{this.state.expression}</div>
         </div>
         <div className='Calc__buttons'>
-          <div className='Calc__button Calc__button_red Calc__button_FC Calc__button_FR' onClick={this.reset.bind(this)}>AC</div>
-          <div className='Calc__button Calc__button_red Calc__button_SC Calc__button_FR' onClick={this.deleteLast.bind(this)}>CE</div>
+          <div className='Calc__button Calc__button_red Calc__button_FC Calc__button_FR' onClick={this.reset}>AC</div>
+          <div className='Calc__button Calc__button_red Calc__button_SC Calc__button_FR' onClick={this.deleteLast}>CE</div>
           <div className='Calc__button Calc__button_TC Calc__button_FR' onClick={this.pressMathSigns.bind(this, '/')}>/</div>
           <div className='Calc__button Calc__button_FoC Calc__button_FR' onClick={this.pressMathSigns.bind(this, '*')}>*</div>
           <div className='Calc__button Calc__button_FC Calc__button_SR' onClick={this.pressNumbers.bind(this, '7')}>7</div>
@@ -142,9 +145,9 @@ export class Calculator extends Component {
           <div className='Calc__button Calc__button_FC Calc__button_FoR' onClick={this.pressNumbers.bind(this, '1')}>1</div>
           <div className='Calc__button Calc__button_SC Calc__button_FoR' onClick={this.pressNumbers.bind(this, '2')}>2</div>
           <div className='Calc__button Calc__button_TC Calc__button_FoR' onClick={this.pressNumbers.bind(this, '3')}>3</div>
-          <div className='Calc__button Calc__button_right' onClick={this.pressEqualSign.bind(this)}>=</div>
+          <div className='Calc__button Calc__button_right' onClick={this.pressEqualSign}>=</div>
           <div className='Calc__button Calc__button_bottom' onClick={this.pressNumbers.bind(this, '0')}>0</div>
-          <div className='Calc__button Calc__button_TC Calc__button_FiR' onClick={this.pressPoint.bind(this)}>.</div>
+          <div className='Calc__button Calc__button_TC Calc__button_FiR' onClick={this.pressPoint}>.</div>
         </div>
       </div>
     )
